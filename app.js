dcfcf0c24dae45030bfe8cfe8531af40a06bf8c9
MuriloSantosdeLima <66491812+MuriloSantosdeLima@users.noreply.github.com>
2021-05-03 17:51:17 -0300

    loginhtml

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-
+const path = require('path');
+const handlebars = require('express-handlebars');
+const { use } = require('./routes/admin');
 const adminRoute = require('./routes/admin');
 
+
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+app.use(express.static(path.join(__dirname,'public') ) );
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,19 @@
 const express = require('express');
-//const app = express();
 const route = express.Router();
 
-route.get('/',(req,res,next) => {
-
-    res.status(200).send({resultado: "estou aqui"});
-
+route.get('/login',(req,res,next) => {
+    res.render("login");
 });
+route.post('/login',(req,res,next) => {
+    
+    const valores = [];
+    valores.push( req.body.login );
+    valores.push( req.body.passw );
+    res.send(valores);
+});
+route.get('',(req,res,next)=>{
+    res.render('login');
+});
+
 
 module.exports = route;
